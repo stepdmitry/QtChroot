2681296b321783ca7d6febfae655358c55380f4f
Dmitry Stepanov <dmitry@localhost.localdomain>
2017-01-12 00:42:58 +0300

    Исправлена ошибка, устранён лишний сигнал))

--- main.cpp
+++ main.cpp
@@ -6,7 +6,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    //QPushButton pushButton("Quit");
-    //QObject::connect(&pushButton, SIGNAL(clicked()), &a, SLOT(quit()));
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QProcess>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -14,12 +14,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    system("pause");
-}
-
 void MainWindow::on_close_button_clicked()
 {
     this->close();
+}
+
+void MainWindow::on_OK_button_clicked()
+{
+        QProcess* myProcess = new QProcess(this);
+        myProcess->start("kate");//попробуйте так, затем kate
+        myProcess->waitForFinished(-1);
 }
