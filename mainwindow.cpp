339418406ffeb50e4d362b7a7f1a93fd8a45ff7c
Dmitry Stepanov <dmitry@localhost.localdomain>
2017-01-12 02:19:18 +0300

    Тестовая запись файла

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QProcess>
+#include <QFile>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -21,7 +22,12 @@ void MainWindow::on_close_button_clicked()
 
 void MainWindow::on_OK_button_clicked()
 {
-        QProcess* myProcess = new QProcess(this);
+    QFile file("cr.sh");
+    if (!file.open(QIODevice::WriteOnly))
+        return;
+    file.write("testtext");
+    file.close();
+        /*QProcess* myProcess = new QProcess(this);
         myProcess->start("kate");//попробуйте так, затем kate
-        myProcess->waitForFinished(-1);
+        myProcess->waitForFinished(-1);*/
 }
